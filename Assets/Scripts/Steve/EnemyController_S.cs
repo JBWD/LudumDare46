38bbdcaf9ff8ad5a0e3405d8367f265438f078bf
Steve Shannon <srshannon@bis.midco.net>
2020-04-18 01:11:52 -0500

    - Started on EnemyController script. Will implement Enemy Spawner as well.

---
+++ Assets/Scripts/Steve/EnemyController_S.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class EnemyController : MonoBehaviour
+{
+    public float moveSpeed = .06f;
+    public float health = 10;
+
+    private Rigidbody2D rbody;
+
+    // Start is called before the first frame update
+    void Start_S()
+    {
+        rbody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update_S()
+    {
+        
+    }
+}
