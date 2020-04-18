18a03238e7f4487a9b9fc345ce9e529005865d15
Weston Glasser <35278058+JBWD@users.noreply.github.com>
2020-04-17 19:33:02 -0500

    Setup

---
+++ Assets/Scripts/_Base/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class PlayerController : MonoBehaviour
+{
+    private void Awake()
+    {
+        Awake_S();
+        Awake_W();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Start_S();
+        Start_W();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Update_S();
+        Update_W();
+    }
+}
