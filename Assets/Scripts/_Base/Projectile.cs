a24ceffe73390ef0d7a866e9951fd3ca8358294c
Weston Glasser <35278058+JBWD@users.noreply.github.com>
2020-04-17 23:24:41 -0500

    Weapon projectile

---
+++ Assets/Scripts/_Base/Projectile.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public partial class Projectile : MonoBehaviour
+{
+    
+    
+    private Rigidbody2D _rb;
+    public float ProjectileSpeed = 1;
+    public AudioClip destructionSound;
+    public GameObject destructionPrefab;
+
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        _rb.velocity = new Vector2(ProjectileSpeed, 0);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            GameObject obj = Instantiate(destructionPrefab, transform.position, Quaternion.identity);
+            Destroy(obj, 2);
+            if (destructionSound != null)
+            {
+                CCS.SoundPlayer.SoundManager.Instance.PlaySound(CCS.SoundPlayer.MixerPlayer.Explosions, destructionSound);
+            }
+            Destroy(gameObject);
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/_Base/Weapon.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CCS.SoundPlayer;
+
+public partial class Weapon : MonoBehaviour
+{
+    public enum FireMode
+    { 
+        OneLane,
+        TwoLanes,
+        ThreeLanes
+    }
+
+
+    public float firerate;
+    public GameObject ProjectilePrefab;
+    public FireMode fireMode;
+    public KeyCode FireKey;
+    public AudioClip FireSound;
+    public float LaneDistance;
+
+
+    private float fireTime = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fireTime = 0;
+    }
+
+    // Update is called once per frame
+    public virtual void Update()
+    {
+        fireTime += Time.deltaTime;
+        if(fireTime > firerate && Input.GetKeyDown(FireKey))
+        {
+            Fire();
+            fireTime = 0;
+        }
+    }
+
+    public virtual void Fire()
+    {
+        if(FireSound != null)
+            SoundManager.Instance.PlaySound(MixerPlayer.Instantiations, FireSound);
+        switch (fireMode)
+        {
+            case FireMode.OneLane:
+                Instantiate(ProjectilePrefab, transform.position, Quaternion.identity);
+                break;
+            case FireMode.TwoLanes:
+                Instantiate(ProjectilePrefab, transform.position + new Vector3(0, LaneDistance), Quaternion.identity);
+                Instantiate(ProjectilePrefab, transform.position + new Vector3(0, -LaneDistance), Quaternion.identity);
+                break;
+            case FireMode.ThreeLanes:
+                Instantiate(ProjectilePrefab, transform.position, Quaternion.identity);
+                Instantiate(ProjectilePrefab, transform.position + new Vector3(0, LaneDistance), Quaternion.identity);
+                Instantiate(ProjectilePrefab, transform.position + new Vector3(0, -LaneDistance), Quaternion.identity);
+                break;
+            
+        }
+        
+    }
+}
